a45309069338f073166abb2d3f1c9890a799df3f
Jan Domingo <janaarondomingo@gmail.com>
2018-12-26 20:44:09 -0800

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Jan  Domingo on 12/26/18.
+//  Copyright © 2018 Jan  Domingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
